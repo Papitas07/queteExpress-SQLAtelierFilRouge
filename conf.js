2732eddba21437919027c692432f9d13981d858b
Fernandez Emilie <emilie.fernandez1@hotmail.com>
2019-06-03 15:06:49 +0200

    suppr fichier conf

--- conf.js
+++
@@ -1,8 +0,0 @@
-const  mysql = require('mysql');
-const  connection = mysql.createConnection({
-host :  'localhost', // adresse du serveur
-user :  'root', // le nom d'utilisateur
-password :  'Nplify400!', // le mot de passe
-database :  'movies_db' // le nom de la base de données
-});
-module.exports = connection;
